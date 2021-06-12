317aab8bfa6312537fd9c291b2a211e51f0a2f3b
Odelon Jhey Pacalso <odjhey@gmail.com>
2021-06-12 12:29:29 +0800

    add POST and stuff

--- main.go
+++ main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func homePage(w http.ResponseWriter, r *http.Request) {
@@ -13,25 +16,64 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequest() {
-	http.HandleFunc("/", homePage)
-	http.HandleFunc("/articles", returnAllArticles)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+
+	myRouter := mux.NewRouter().StrictSlash(true)
+
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("/article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", returnSingleArticle)
+	myRouter.HandleFunc("/hello", returnHello).Methods("POST")
+	myRouter.HandleFunc("/hello", returnHello)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func returnHello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, `{"yo": "brow"}`)
 }
 
 func main() {
+	fmt.Println("Rest API v2.0 - Mux Routers")
+
 	Articles = []Article{
-		{Title: "t1", Desc: "d", Content: "content123213"},
-		{Title: "t12", Desc: "d1", Content: "content123213asdf"},
+		{Id: "1", Title: "t1", Desc: "d", Content: "content123213"},
+		{Id: "2", Title: "t12", Desc: "d1", Content: "content123213asdf"},
 	}
 	handleRequest()
 }
 
 func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Articles hit")
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(Articles)
 }
 
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	fmt.Println(vars)
+	fmt.Fprint(w, "Key: "+key)
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+
+	Articles = append(Articles, article)
+
+	json.NewEncoder(w).Encode(article)
+}
+
 type Article struct {
+	Id      string `json:"id"`
 	Title   string `json:"title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
