e7f43e5ed719a2ebcf880964dd2085e3c3a298e9
Odelon Jhey Pacalso <odjhey@gmail.com>
2021-06-13 10:18:07 +0800

    update message to check swagger

--- handlers/yaharu_handlers.go
+++ handlers/yaharu_handlers.go
@@ -10,8 +10,10 @@ func GetYaharu(w http.ResponseWriter, r *http.Request) {
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "welcome to the homepage")
-	fmt.Println("endpoitnn hit")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "welcome to the homepage <br>")
+	fmt.Fprint(w, `go check the <a href="/swagger/index.html">swagger UI</a>`)
+	fmt.Println("endpoint hit")
 }
 
 func ReturnSSS(w http.ResponseWriter, r *http.Request) {
